8c742c388f90d189825f806329b597e847decef9
阿海 <xu15222379857@gmail.com>
2024-06-11 01:42:46 +0800

    Remove no used code

--- src/handler.ts
+++ src/handler.ts
@@ -5,11 +5,6 @@ const PROXY_HEADER_ALLOW_LIST: string[] = ["accept", "user-agent", "accept-encod
 
 const validActionNames = new Set(["manifests", "blobs", "tags", "referrers"])
 
-// const ORG_NAME_BACKEND:{ [key: string]: string; } = {
-//   "gcr": "https://gcr.io",
-//   "k8sgcr": "https://k8s.gcr.io",
-//   "quay": "https://quay.io",
-// }
 
 const DEFAULT_BACKEND_HOST: string = "https://registry-1.docker.io"
 
@@ -28,39 +23,22 @@ function copyProxyHeaders(inputHeaders: Headers) : Headers {
 }
 
 function orgNameFromPath(pathname: string): string|null {
-  // const splitedPath: string[] = pathname.split("/", 3)
-  // if (splitedPath.length === 3 && splitedPath[0] === "" && splitedPath[1] === "v2") {
-  //   return splitedPath[2].toLowerCase()
-  // }
   return null
 }
 
 function hostByOrgName(orgName: string|null): string {
-  // if (orgName !== null && orgName in ORG_NAME_BACKEND) {
-  //   return ORG_NAME_BACKEND[orgName]
-  // }
   return DEFAULT_BACKEND_HOST
 }
 
 function rewritePath(orgName: string | null, pathname: string): string {
   let splitedPath = pathname.split("/");
 
-  // /v2/repo/manifests/xxx -> /v2/library/repo/manifests/xxx
-  // /v2/repo/blobs/xxx -> /v2/library/repo/blobs/xxx
   if (orgName === null && splitedPath.length === 5 && validActionNames.has(splitedPath[3])) {
     splitedPath = [splitedPath[0], splitedPath[1], "library", splitedPath[2], splitedPath[3], splitedPath[4]]
   }
 
   return splitedPath.join("/")
 
-  // if (orgName === null || !(orgName in ORG_NAME_BACKEND)) {
-  //   return pathname
-  // }
-  //
-  // const cleanSplitedPath = splitedPath.filter(function(value: string, index: number) {
-  //   return value !== orgName || index !== 2;
-  // })
-  // return cleanSplitedPath.join("/")
 }
 
 async function handleRegistryRequest(request: Request): Promise<Response> {
